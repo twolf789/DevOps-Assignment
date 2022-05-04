c4d06429db67761dc63d97063fba8afef90f7341
twolf789 <98260230+twolf789@users.noreply.github.com>
2022-05-04 12:05:38 +0300

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -4,9 +4,3 @@
 sudo apt install curl
 curl -fsSL get.docker.com -o get-docker.sh
 sh get-docker.sh
-
-# Adding the user to the 'docker' group in order to avoid writing 'sudo' in the beginning of each docker command
-if [[ $# -eq 1 ]]; then
-  sudo usermod -aG docker $1
-else
-  echo "Sorry, but you didn't provide your username as a positional argument. Docker had been installed, but your user wasn't added to the docker group."
