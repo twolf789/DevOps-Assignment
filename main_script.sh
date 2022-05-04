9f533d29490997ceb23812678ce70b0748300a7c
twolf789 <98260230+twolf789@users.noreply.github.com>
2022-05-04 11:12:28 +0300

    Update main_script.sh

--- main_script.sh
+++ main_script.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Run VPN server based on the configuration we did before, on the first container
 docker run -v $PWD/vpn_files:/etc/openvpn -d -p 2998:1194/udp --net yuvals_app_bridge --name container1_vpnserver  --cap-add=NET_ADMIN kylemanna/openvpn
 
