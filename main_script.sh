2e75ff468cb2dea318911fff2b794e1549143c3e
twolf789 <98260230+twolf789@users.noreply.github.com>
2022-05-04 10:38:29 +0300

    Create main_script.sh

---
+++ main_script.sh
@@ -0,0 +1,5 @@
+# Run VPN server based on the configuration we did before, on the first container
+docker run -v $PWD/vpn_files:/etc/openvpn -d -p 2998:1194/udp --net yuvals_app_bridge --name container1_vpnserver  --cap-add=NET_ADMIN kylemanna/openvpn
+
+# Create second container in the same network we created.
+docker run -d --net yuvals_app_bridge -p 1001:80 --name container2 nginx
